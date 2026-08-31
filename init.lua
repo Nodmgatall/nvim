a6c7b055b4bba004936c7200cb8da6ff912f681f
Oliver Heidmann <oliver.heidmann@mpimet.mpg.de>
2026-08-31 13:24:37 +0200

    selected colorscheme, changed location of setting mapleader

--- init.lua
+++ init.lua
@@ -1,7 +1,14 @@
-print("init.lua loaded")
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("habamax")
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 
 require("config.lazy")
 require("config.keybinds")
-
